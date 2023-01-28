bb00db38b4e7cb03fefe8e77ee490fd875aa0238
iansw246 <ianjazz246@gmail.com>
2023-01-27 16:12:40 -0800

    Use Autocomplete instead of select

--- src/components/CountrySelector.tsx
+++ src/components/CountrySelector.tsx
@@ -1,28 +1,43 @@
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { Autocomplete, AutocompleteProps, TextField } from "@mui/material";
 import { useMemo } from "react";
 
 import COUNTRY_NAME_TO_CODE from "../data/countryCodes";
 
-function createCountryMenuItems(countryNameToCode: Map<string, string>): JSX.Element[] {
-    const result: JSX.Element[] = [];
-    // Should use for of loop over .entries() or .keys()
-    // But to support older browsers, forEach is used instead.
-    // Otherweise, we receive the error:
-    // Type 'Generator<Element, void, unknown>' can only be iterated through when using the '--downlevelIteration' flag or with a '--target' of 'es2015' or higher.
+const DEFAULT_COUNTRY_CODE: string = "US";
+
+interface CountryType {
+    label: string;
+    code: string;
+}
+
+function makeCountryOptions(countryNameToCode: Map<string, string>): CountryType[] {
+    /**
+     * Should use for of loop over .entries() or .keys()
+     * But to support older browsers, forEach is used instead.
+     * Otherweise, we receive the error:
+     * Type 'Generator<Element, void, unknown>' can only be iterated through when using the '--downlevelIteration' flag or with a '--target' of 'es2015' or higher.
+     */
+    const options: CountryType[] = [];
     countryNameToCode.forEach((countryCode, countryName) => {
-        result.push(<MenuItem value={countryCode} key={countryCode}>{countryName}</MenuItem>);
+        options.push({ label: countryName, code: countryCode });
     });
-    return result;
+    return options;
 }
 
 export default function CountrySelector() {
-    const countryMenuItems: JSX.Element[] = useMemo(() => createCountryMenuItems(COUNTRY_NAME_TO_CODE), []);
+    // const countryMenuItems: JSX.Element[] = useMemo(() => createCountryMenuItems(COUNTRY_NAME_TO_CODE), []);
+    const countryOptions = useMemo(() => {
+        return makeCountryOptions(COUNTRY_NAME_TO_CODE);
+    }, []);
+    const defaultCountryOption = useMemo(() => {
+        return countryOptions.find((option) => option.code === DEFAULT_COUNTRY_CODE);
+    }, [countryOptions]);
     return (
-        <FormControl>
-            <InputLabel>Country</InputLabel>
-            <Select defaultValue="US">
-                {countryMenuItems}
-            </Select>
-        </FormControl>
+        <Autocomplete 
+            options={countryOptions}
+            autoHighlight
+            defaultValue={defaultCountryOption}
+            renderInput={(params) => <TextField {...params} label="Country" />}
+        />
     );
 }
