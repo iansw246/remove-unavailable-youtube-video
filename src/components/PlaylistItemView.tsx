f30e9a28167b0d0ae479dca3e8018a6b92714ea3
ianjazz246 <ianjazz246@gmail.com>
2023-06-03 16:20:04 -0700

    Make channel name in video list smaller

--- src/components/PlaylistItemView.tsx
+++ src/components/PlaylistItemView.tsx
@@ -14,7 +14,7 @@ export default function PlaylistItemView({playlistItem}: Props) {
             <YouTubeThumbnail thumbnailURL={getThumbnailURL(playlistItem.snippet?.thumbnails)} alt={playlistItem.snippet?.title + " thumbnail"} sx={{marginRight: 1}} />
             <Box minWidth="200px">
                 <Typography><NewTabLink href={makeVideoURL(playlistItem.contentDetails?.videoId ?? "", playlistItem.snippet?.playlistId, playlistItem.snippet?.position)}>{playlistItem.snippet?.title}</NewTabLink></Typography>
-                <Typography><NewTabLink href={makeChannelURL(playlistItem.snippet?.videoOwnerChannelId ?? "")}>{playlistItem.snippet?.videoOwnerChannelTitle}</NewTabLink></Typography>
+                <Typography variant="body2"><NewTabLink href={makeChannelURL(playlistItem.snippet?.videoOwnerChannelId ?? "")}>{playlistItem.snippet?.videoOwnerChannelTitle}</NewTabLink></Typography>
                 <Typography>Published on {(new Date(playlistItem.snippet?.publishedAt || "")).toLocaleString()}</Typography>
             </Box>
         </Box>
