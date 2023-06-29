9d2481939af354fee5ecac98a5b95f23483b1598
ianjazz246 <ianjazz246@gmail.com>
2023-06-29 11:04:34 -0700

    Add smoke test for App

---
+++ src/App.test.tsx
@@ -0,0 +1,7 @@
+import App from "./App";
+import ReactDOM from "react-dom";
+
+it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+});
