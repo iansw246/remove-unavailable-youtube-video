d221537b356d4f056405feb4980ac3615912d829
iansw246 <35947093+iansw246@users.noreply.github.com>
2023-10-31 18:04:15 -0700

    Add tests

---
+++ src/components/PlaylistList.test.tsx
@@ -0,0 +1,18 @@
+import PlaylistList from "./PlaylistList";
+import playlistListResponse from "../testData/playlistsListResponse";
+import { render, screen } from "@testing-library/react";
+
+const playlists = playlistListResponse.items;
+
+test("PlaylistList displays playlists", () => {
+    render(<PlaylistList playlists={playlists} onGetUnavailableItemsClick={() => {}}/>);
+    for (const playlist of playlists) {
+        expect(screen.getByText(playlist.snippet.title)).toBeInTheDocument();
+    }
+});
+
+// test("PlaylistList displays playlists", () => {
+//     const onePlaylist = playlists.slice(1);
+//     const handleGetUnavailableItems = jest.fn();
+//     render(<PlaylistList playlists={onePlaylist} onGetUnavailableItemsClick={() => {}}/>);
+// });
